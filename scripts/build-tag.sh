67e2dd1513b420d54a237a56f13ce841be19273e
howdy39 <tatsuya.howdy@gmail.com>
2019-02-08 15:25:47 +0900

    fix: CI

--- scripts/build-tag.sh
+++ scripts/build-tag.sh
@@ -7,14 +7,13 @@ echo "--------------"
 git config user.email "tatsuya.howdy@gmail.com"
 git config user.name "howdy39"
 
-git reset --hard HEAD
 git checkout $CIRCLE_TAG
 git tag -d $CIRCLE_TAG
 git push origin :refs/tags/$CIRCLE_TAG
 
 yarn run build
 git add dist
-git commit -m "build $CIRCLE_TAG by CI"
+git commit -m "build $CIRCLE_TAG by CirclieCI"
 
 git tag $CIRCLE_TAG
 git push origin $CIRCLE_TAG
