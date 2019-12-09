dfd4e29d09d250238bc1a9b54c274a29571d0dc3
howdy39 <howdy39@stores.jp>
2019-12-09 15:17:34 +0900

    add : fetch

--- scripts/release-beta.sh
+++ scripts/release-beta.sh
@@ -9,13 +9,16 @@ echo "--------------"
 git config user.email "tatsuya.howdy@gmail.com"
 git config user.name "howdy39"
 
+git fetch
 git checkout beta
+cat dist/piyo.txt
 git merge origin dashboard-renewal
 
 rm -Rf dist
 git commit -m "ビルド結果を削除 by CircleCI"
 
 yarn run build
+cat dist/piyo.txt
 git add dist
 git commit -m "ビルド結果を登録 by CircleCI"
 
